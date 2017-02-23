c66070362f5538adad878da8cc2de88338b3605f
Andrew Kerwin <akerwin@carroll.edu>
2017-02-22 22:24:07 -0700

    Button presses functionally

--- SetGame/app/controllers/Application.java
+++ SetGame/app/controllers/Application.java
@@ -2,13 +2,19 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import models.Task;
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Pink Bunny Slippers"));
     }
 
+    public static Result addTask() {
+    	play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
+    	models.Task task = form.get();
+    	task.save();
+    	return redirect(routes.Application.index());
+    }
 }
